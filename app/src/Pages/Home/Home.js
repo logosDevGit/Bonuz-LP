cce72d68de5a848d34b6e32e88fc8815cb472f03
JoaoVitorLima242 <jlima0500@gmail.com>
2022-04-25 15:48:40 -0300

    starting application

---
+++ app/src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../assets/styles/global.css'
+
+
+const Home = () => {
+
+    return(
+        <>
+            <h1>bora</h1>
+        </>
+    )
+}
+
+export default Home;
