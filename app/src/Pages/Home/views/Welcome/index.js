d003efce1990cd2bf82e93c0e9f8e206d7d90d7a
JoaoVitorLima242 <jlima0500@gmail.com>
2022-04-27 09:42:26 -0300

    adjusting ancor rel

--- app/src/Pages/Home/views/Welcome/index.js
+++ app/src/Pages/Home/views/Welcome/index.js
@@ -7,7 +7,7 @@ const Welcome = () => {
             <div className="cont-1">
                 <h1 className="my-4">Simplifique sua vida <span>contábil</span>.</h1>
                 <p className="my-4">Tudo o que você precisa para estar em dia com a contabilidade, impostos e obrigações legais da sua empresa para no final do dia dormir tranquilo.</p>
-                <a className="btn btn-red my-4" target="_blank" href="https://hubscontabilidade.com.br/clubehubs/">Consultar planos</a>
+                <a className="btn btn-red my-4" rel="noreferrer" target="_blank" href="https://hubscontabilidade.com.br/clubehubs/">Consultar planos</a>
             </div>
             <div>
                 <img src={Owl} alt='Coruja aplicativo Hub'/>
