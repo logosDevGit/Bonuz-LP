edbe0857f33d993928a0d82a0e41021c58b765ea
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-19 11:56:19 -0300

    change media

--- app/src/Pages/Home/views/Hubs/index.js
+++ app/src/Pages/Home/views/Hubs/index.js
@@ -8,7 +8,7 @@ const Hubs = () => {
             <StyledTitle title={"Hubs"}/>
             <p>Descomplicando o dia dia do pequeno e médio empreendedor a partir da contabilidade digital. Usamos a tecnologia para proporcionar a melhor experiência  para você. Tiramos todas as suas dúvidas e ajudamos você a tomar as melhores decisões para o seu negócio.</p>
             <div className="media">
-                <iframe src="http://www.youtube.com/embed/xDMP3i36naA"/>
+                <iframe src="https://www.youtube.com/embed/cWDJoK8zw58"/>
             </div>
         </div>
     )
